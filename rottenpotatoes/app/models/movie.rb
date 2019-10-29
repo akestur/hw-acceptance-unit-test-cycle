8750df784b3a8af83fe830e09f4ca4145693b457
Akhil Kestur <akhilkestur@Akhils-MacBook-Pro-2.local>
2019-10-28 21:39:26 -0700

    completed passing cucumber

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+
+  def self.similar_movies (movie_title)
+    director = Movie.find_by(title: movie_title).director
+    if director.blank?
+      return nil
+    end
+    Movie.where(director: director).all.map(&:title)
+  end
 end
